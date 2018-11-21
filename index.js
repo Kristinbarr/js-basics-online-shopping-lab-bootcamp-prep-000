270350824e3e062ddf6ffda9cf9158f22011e748
Kristin Barr <kristinbarr@gmail.com>
2018-11-21 23:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    console.log(`Sorry, we don't have a credit card on file for you.`)
+    return `Sorry, we don't have a credit card on file for you.`;
   } else {
     let totalCost = total();
     cart.splice(0,cart.length);
